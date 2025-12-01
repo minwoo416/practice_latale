5a5cd6f758588521ab570c8f804628dd75fa9a85
minwoo <jmw041615@gmail.com>
2025-12-01 18:16:17 +0900

    2행 3행 완성

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,12 @@ var banner = new Swiper("main .banner_container .swiper", {
         el: "main .banner_container .swiper .swiper_btn_wrap .swiper-pagination",
     },
 });
+var lataleVideo = new Swiper("main .latale_video_container .swiper", {
+    navigation:{
+        nextEl:"main .latale_video_container .swiper-button-next",
+        prevEl:"main .latale_video_container .swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+})
